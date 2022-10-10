c4d0d1f70efd8eff03a415ff753819e06d4cce0a
Biribin <lineo.biribin@efrei.net>
2022-10-10 16:36:38 +0200

    création des classes

---
+++ back/src/main/java/efrei/Chaussures.java
@@ -0,0 +1,53 @@
+
+
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+
+public class Chaussures {
+
+    Int id;
+    String nom;
+    String couleur;
+    Int prix;
+
+    public Chaussures(Int id, String nom, String couleur, Int prix){
+        this.id = id;
+        this.nom = nom;
+        this.couleur = couleur;
+        this.prix= prix;
+    }
+    public Int getId() {
+        return id;
+    }
+    public void setId(Int id) {
+        this.id = id;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getCouleur() {
+        return couleur;
+    }
+
+    public void setCouleur(String couleur) {
+        this.couleur = couleur;
+    }
+    public Int getPrix() {
+        return prix;
+    }
+
+    public void setPrix(Int prix) {
+        this.prix = prix;
+    }
+
+}
+    
+  
